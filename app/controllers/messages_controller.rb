6dd066bc19f5b93bb80d08a2198a9a5b22820ea2
Melisa Budini <melixbq@gmail.com>
2020-12-09 22:24:51 -0300

    Add: create username

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,7 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
-
+    @message.user = current_user
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
